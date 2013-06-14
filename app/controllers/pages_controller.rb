080b828c138ad8fd7aab6d117fdd8eb0acd3f8bd
Chris Lucas <christopher.lucas@gmail.com>
2013-06-14 15:46:52 -0400

    routes and pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
-	
-	def action
-		end
+
+  def about
+  end
+
+  def schedule
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
-Aes::Application.routes.draw do
+Omrails::Application.routes.draw do
+  get 'about'=> 'pages#about'
+
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
